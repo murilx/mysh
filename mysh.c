e4d35d0c1a77bdf532f8665990bc8d165b45e8b4
Murilo Geraldini <murilogeraldini@unesp.br>
2023-03-14 17:43:32 -0300

    Criação da função split_text
    Criação da função split_text que divide uma string e retorna um vetor
    de strings. Função utilizada para criar o vetor de argumentos
    requisitado pela função execv()

--- mysh.c
+++ mysh.c
@@ -2,20 +2,49 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/queue.h>
 
 #define CWD_SIZE 100
+
+char **split_text(char *);
 
 int main() {
     do {
         char user_input[100];
+        char **arguments = NULL;
         
         // O prompt no formato expecificado em (6)
         printf("[MySh] %s@%s:%s$ ", getenv("USER"), getenv("HOSTNAME"), getcwd(NULL, CWD_SIZE));
         fgets(user_input, sizeof(user_input), stdin);
 
+        // Divide a entrada recebida em um vetor de strings
+        arguments = split_text(user_input);
+
+        // Verifica se foi possível dividir os valores de entrada
+        if(arguments == NULL)
+            exit(-1);
         
-        
+        /* execvp(arguments[0], arguments); */
+
+        // Libera a memória que foi alocada
+        if(arguments != NULL)
+            free(arguments);
     } while(1);
     return 0;
+}
+
+char **split_text(char *str) {
+    char **list = NULL;
+    char *token = strtok(str, " ");
+    int n_spaces = 0;
+
+    // Armazena o comando e os argumentos em uma array de strings
+    while(token != NULL) {
+        list = realloc(list, sizeof(char *) * ++n_spaces);
+        if(list == NULL)
+            exit(-1); // Falha na alocação de memória
+        list[n_spaces-1] = (char *)malloc(sizeof(char *) * strlen(token) + 1);
+        strcpy(list[n_spaces-1], token);
+        token = strtok(NULL, " ");
+    }
+    return list;
 }
