63e330c1732886ef47afcb045e61330248fa5536
Murilo Geraldini <murilogeraldini@unesp.br>
2023-03-14 21:29:41 -0300

    Verificação do status_code do programa executado
    Faz a verificação do status_code do programa executado por execvp e
    caso não tenha executado, mostra uma mensagem de erro na
    tela.
    
    Adicionado também a função wait que faz que o processo pai
    espere o processo filho terminar de ser executado para voltar a
    executar. Isso é necessário, entre outros motivos, para que a ordem em
    que as informações são mostradas pelo shell seja correta, no caso as
    informações do programa que está sendo executado devem ser mostradas
    antes do prompt do shell, o que não estava acontecendo antes da adição
    de wait(NULL) no código.

--- mysh.c
+++ mysh.c
@@ -2,17 +2,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define CWD_SIZE 100
 
-char **split_text(char *);
 char **split_text(char *str, const char *delim);
 
 int main() {
-    char user_input[100];
     char user_input[1000];
     char **arguments = NULL;
-        
+    int status_code = 0;
+    
     do {
         // O prompt no formato expecificado em (6)
         printf("[MySh] %s@%s:%s$ ", getenv("USER"), getenv("HOSTNAME"), getcwd(NULL, CWD_SIZE));
@@ -23,8 +24,6 @@ int main() {
             user_input[strlen(user_input)-1] = '\0';
         
         // Divide a entrada recebida em um vetor de strings
-        arguments = split_text(user_input);
-
         arguments = split_text(user_input, " ");
         
         // Verifica se foi possível dividir os valores de entrada
@@ -32,14 +31,16 @@ int main() {
             exit(-1);
 
         // Verifica por comandos especiais
-        if(strcmp(arguments[0], "exit\n") == 0)
         if(strcmp(arguments[0], "exit") == 0)
             break;
 
         // Executa o comando enviado pelo usuário em um processo filho
         if(fork() == 0) {
-            execvp(arguments[0], arguments);
+           status_code = execvp(arguments[0], arguments);
+           if(status_code == -1)
+               fprintf(stderr, "Erro na execucao de: %s\n", arguments[0]);
         }
+        wait(NULL); // Espera o processo filho terminar para continuar o processo pai
     } while(1);
 
     // Libera a memória que foi alocada
@@ -48,6 +49,7 @@ int main() {
     return 0;
 }
 
+char **split_text(char *str, const char *delim) {
     char **list = NULL;
     char *token = strtok(str, delim);
     int n_spaces = 0;
